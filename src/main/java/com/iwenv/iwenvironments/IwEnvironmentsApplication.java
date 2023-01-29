cbc244d2c47933bc845bfa9a641d36602cc846b1
Vinlyreeks <enwerevincent@gmail.com>
2023-01-29 13:44:12 +0100

    Auth-dev profile

---
+++ src/main/java/com/iwenv/iwenvironments/IwEnvironmentsApplication.java
@@ -0,0 +1,13 @@
+package com.iwenv.iwenvironments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwEnvironmentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IwEnvironmentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iwenv/iwenvironments/IwEnvironmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iwenv.iwenvironments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IwEnvironmentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
